f8f270864a8998d3f3005e64c0f73eb27f2ca122
Sergio Galarza <sergiowgp@outlook.com>
2025-06-07 18:22:53 -0500

    back in add

--- src/add.py
+++ src/add.py
@@ -1,25 +1,27 @@
 import flet as ft
-from grid import load_grid
 
 def add_classroom(page: ft.Page):
-    def close_dialog(e):
-        page.close(add_dialog)
-        page.update()
 
     def new_dialog(e):
+        from grid import load_grid
         page.client_storage.set(f"Aula {classroom_number_field.value}",0)
         page.close(add_dialog)
         load_grid(page)
         page.update()
 
-    classroom_number_field = ft.TextField(label="N° de salon", value="", keyboard_type=ft.KeyboardType.NUMBER)
+    classroom_number_field = ft.TextField(
+        label="N° de salon",
+        value="",
+        keyboard_type=ft.KeyboardType.NUMBER
+    )
+
     add_dialog = ft.AlertDialog(
         modal=True,
         title=ft.Text("Agregar Salon"),
         content=classroom_number_field,
         actions=[
+            ft.TextButton("Cerrar", on_click=lambda e: page.close(add_dialog)),
             ft.TextButton("Guardar", on_click=new_dialog),
-            ft.TextButton("Cerrar", on_click=close_dialog),
         ],
         actions_alignment=ft.MainAxisAlignment.END,
     )
